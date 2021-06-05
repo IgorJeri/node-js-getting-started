1a027bf1af359cf857014b39068c60f9383e5bce
IgorJeri <43531011+IgorJeri@users.noreply.github.com>
2021-06-05 15:41:07 +1000

    basics

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+import * as echarts from 'echarts';
+import * as jsdom from 'jsdom'
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/chart', (req, res) => {
+
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      status: 200,
+      body: 'hi'
+
+
+    };
+
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
